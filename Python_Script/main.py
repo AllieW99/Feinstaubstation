74bb38346e1f87c75930bac9c7c6b22631be80d9
Allie Weitenkamp <allie.weitenkamp@gdata.de>
2023-03-01 10:11:45 +0100

    add .sqlite to database

--- Python_Script/main.py
+++ Python_Script/main.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 print("Please enter date in format YYYY-MM-DD:")
-timestamp = "2022-03-14" # input()
+timestamp = "2022-03-14"  # input()
 print("Please enter wanted datatype out of Feinstaub_P1/Feinstaub_P2/Temperatur/Luftfeuchtigkeit:")
-datatype = "Feinstaub_P1" # input()
+datatype = "Feinstaub_P1"  # input()
 abfrage = ""
 
 if datatype == "Feinstaub_P1":
@@ -26,10 +26,12 @@ if datatype == "Luftfeuchtigkeit":
                'FROM Temperatur_Luftdruck '
                'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
 
-con = sqlite3.connect("../Database/Feinstaubprojekt")
+con = sqlite3.connect("../Database/Feinstaubprojekt.sqlite")
 cur = con.cursor()
 
 for row in cur.execute(abfrage):
     print("Min: ", round(row[0], 2))
     print("Max: ", round(row[1], 2))
     print("Avg: ", round(row[2], 2))
+
+con.close()
