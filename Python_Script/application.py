1136e408cf4263a61f623efd70578a85715a9c16
Allie Weitenkamp <allie.weitenkamp@gdata.de>
2023-03-02 14:14:57 +0100

    add application

---
+++ Python_Script/application.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter import ttk
+from main import query_database
+
+
+def get_data():
+    try:
+        date_value = date.get()
+        datatype_value = datatype.get()
+        data_array = query_database(date_value, datatype_value)
+        minimum.set(data_array[0])
+        maximum.set(data_array[1])
+        average.set(data_array[2])
+    except ValueError:
+        pass
+
+
+root = Tk()
+root.title("Feinstaubdaten")
+
+mainframe = ttk.Frame(root, padding="5 5 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+date = StringVar()
+date_entry = ttk.Entry(mainframe, width=10, textvariable=date)
+date_entry.grid(column=1, row=1, sticky=(W, E))
+
+datatype = StringVar()
+datatype_entry = ttk.Combobox(mainframe, textvariable=datatype, values=('Feinstaub P1', 'Feinstaub P2', 'Temperatur', 'Luftfeuchtigkeit'))
+datatype_entry.grid(column=1, row=2, sticky=(W, E))
+
+minimum = StringVar()
+ttk.Label(mainframe, textvariable=minimum).grid(column=1, row=3, sticky=E)
+
+maximum = StringVar()
+ttk.Label(mainframe, textvariable=maximum).grid(column=1, row=4, sticky=E)
+
+average = StringVar()
+ttk.Label(mainframe, textvariable=average).grid(column=1, row=5, sticky=E)
+
+ttk.Button(mainframe, text="Get data", command=get_data).grid(column=2, row=6, sticky=W)
+
+ttk.Label(mainframe, text="Date (YYYY-MM-DD)").grid(column=2, row=1, sticky=W)
+ttk.Label(mainframe, text="Datatype").grid(column=2, row=2, sticky=W)
+ttk.Label(mainframe, text="Minimum").grid(column=2, row=3, sticky=W)
+ttk.Label(mainframe, text="Maximum").grid(column=2, row=4, sticky=W)
+ttk.Label(mainframe, text="Average").grid(column=2, row=5, sticky=W)
+
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+date_entry.focus()
+
+root.mainloop()
--- Python_Script/main.py
+++ Python_Script/main.py
@@ -1,37 +1,37 @@
 import sqlite3
 
-print("Please enter date in format YYYY-MM-DD:")
-timestamp = "2022-03-14"  # input()
-print("Please enter wanted datatype out of Feinstaub_P1/Feinstaub_P2/Temperatur/Luftfeuchtigkeit:")
-datatype = "Feinstaub_P1"  # input()
-abfrage = ""
 
-if datatype == "Feinstaub_P1":
-    abfrage = (f'SELECT MIN(P1), MAX(P1), AVG(P1) '
-               'FROM Feinstaub '
-               'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
+def query_database(timestamp, datatype):
+    returned_data = [""] * 3
+    abfrage = ""
 
-if datatype == "Feinstaub_P2":
-    abfrage = (f'SELECT MIN(P2), MAX(P2), AVG(P2) '
-               'FROM Feinstaub '
-               'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
+    if datatype == "Feinstaub P1":
+        abfrage = (f'SELECT MIN(P1), MAX(P1), AVG(P1) '
+                   'FROM Feinstaub '
+                   'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
 
-if datatype == "Temperatur":
-    abfrage = ('SELECT MIN(temperature), MAX(temperature), AVG(temperature) '
-               'FROM Temperatur_Luftdruck '
-               'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
+    if datatype == "Feinstaub P2":
+        abfrage = (f'SELECT MIN(P2), MAX(P2), AVG(P2) '
+                   'FROM Feinstaub '
+                   'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
 
-if datatype == "Luftfeuchtigkeit":
-    abfrage = ('SELECT MIN(humidity), MAX(humidity), AVG(humidity) '
-               'FROM Temperatur_Luftdruck '
-               'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
+    if datatype == "Temperatur":
+        abfrage = ('SELECT MIN(temperature), MAX(temperature), AVG(temperature) '
+                   'FROM Temperatur_Luftdruck '
+                   'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
 
-con = sqlite3.connect("../Database/Feinstaubprojekt.sqlite")
-cur = con.cursor()
+    if datatype == "Luftfeuchtigkeit":
+        abfrage = ('SELECT MIN(humidity), MAX(humidity), AVG(humidity) '
+                   'FROM Temperatur_Luftdruck '
+                   'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
 
-for row in cur.execute(abfrage):
-    print("Min: ", round(row[0], 2))
-    print("Max: ", round(row[1], 2))
-    print("Avg: ", round(row[2], 2))
+    con = sqlite3.connect("../Database/Feinstaubprojekt.sqlite")
+    cur = con.cursor()
 
-con.close()
+    for row in cur.execute(abfrage):
+        returned_data[0] = round(row[0], 2)
+        returned_data[1] = round(row[1], 2)
+        returned_data[2] = round(row[2], 2)
+
+    con.close()
+    return returned_data
