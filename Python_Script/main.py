84762cb31d2fd8b1bcd5a9fc7f8636be6518d4f5
Allie Weitenkamp <allie.weitenkamp@gdata.de>
2023-03-02 15:21:55 +0100

    add units

--- Python_Script/main.py
+++ Python_Script/main.py
@@ -4,21 +4,25 @@ import sqlite3
 def query_database(timestamp, datatype):
     returned_data = [""] * 3
     abfrage = ""
+    einheit = ""
 
     if datatype == "Feinstaub P1":
         abfrage = (f'SELECT MIN(P1), MAX(P1), AVG(P1) '
                    'FROM Feinstaub '
                    'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
+        einheit = " µg/m^3"
 
     if datatype == "Feinstaub P2":
         abfrage = (f'SELECT MIN(P2), MAX(P2), AVG(P2) '
                    'FROM Feinstaub '
                    'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
+        einheit = " µg/m^3"
 
     if datatype == "Temperatur":
         abfrage = ('SELECT MIN(temperature), MAX(temperature), AVG(temperature) '
                    'FROM Temperatur_Luftdruck '
                    'WHERE DATE(timestamp) = DATE(\'{}\')').format(timestamp)
+        einheit = " °C"
 
     if datatype == "Luftfeuchtigkeit":
         abfrage = ('SELECT MIN(humidity), MAX(humidity), AVG(humidity) '
@@ -26,14 +30,15 @@ def query_database(timestamp, datatype):
                    'WHERE DATE(timestamp) = DATE(\'{}\') '
                    'AND humidity > 1 '
                    'AND humidity < 99').format(timestamp)
+        einheit = " %"
 
     con = sqlite3.connect("../Database/Feinstaubprojekt.sqlite")
     cur = con.cursor()
 
     for row in cur.execute(abfrage):
-        returned_data[0] = round(row[0], 2)
-        returned_data[1] = round(row[1], 2)
-        returned_data[2] = round(row[2], 2)
+        returned_data[0] = str(round(row[0], 2)) + einheit
+        returned_data[1] = str(round(row[1], 2)) + einheit
+        returned_data[2] = str(round(row[2], 2)) + einheit
 
     con.close()
     return returned_data
